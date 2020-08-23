b626852f76b4ac12854147de2c5e63ffb4dc97fc
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-08-23 20:26:26 +0200

    add styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,54 @@
 import React from "react";
 import { Link } from "gatsby";
+//import "./header.module.scss";
+import headerStyles from "./header.module.scss";
 
 const Header = () => {
   return (
     <div>
-      <header>
-        <h1>Mihailo Milenkovic</h1>
+      <header className={headerStyles.header}>
+        <h1>
+          <Link to="/" className={headerStyles.title}>
+            Mihailo Milenkovic
+          </Link>
+        </h1>
         <nav>
-          <ul>
+          <ul className={headerStyles.navList}>
             <li>
-              <Link to="/">Home</Link>
+              <Link
+                className={headerStyles.navItem}
+                activeClassName={headerStyles.activeNavItem}
+                to="/"
+              >
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/blog">Blog</Link>
+              <Link
+                className={headerStyles.navItem}
+                activeClassName={headerStyles.activeNavItem}
+                to="/blog"
+              >
+                Blog
+              </Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link
+                className={headerStyles.navItem}
+                activeClassName={headerStyles.activeNavItem}
+                to="/about"
+              >
+                About
+              </Link>
             </li>
             <li>
-              <Link to="/contact">Contact me</Link>
+              <Link
+                className={headerStyles.navItem}
+                activeClassName={headerStyles.activeNavItem}
+                to="/contact"
+              >
+                Contact me
+              </Link>
             </li>
           </ul>
         </nav>
