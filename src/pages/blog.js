91b8aa6fdb6ba9cba5a1c0c43b9abf57b30c317c
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-08-25 19:49:22 +0200

    add pages for markdown posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,42 @@
 import React from "react";
 
 import Layout from "../components/layout";
+import { graphql, useStaticQuery, Link } from "gatsby";
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => {
+          const front = edge.node.frontmatter;
+          return (
+            <li>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{front.title}</h2>
+              </Link>
+              <p>{front.date}</p>
+            </li>
+          );
+        })}
+      </ol>
     </Layout>
   );
 };
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "The Great Gatsby Bootcamp"
+date: "2020-08-25"
+---
+
+I'm going through a new bootcamp
+
+## Topics covered
+
+1. Gatsby
+2. GraphQL
+3. React
