4f63b8fbb74dcf1d15a101cdeed51a6fc282f223
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-08-26 17:39:45 +0200

    add configuration for head

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../components/layout";
+import Head from "../components/head";
 
 const ContactPage = () => {
   return (
     <Layout>
+      <Head title="Contact" />
       <h1>Contact me</h1>
       <p>email: mihailo.milenkovic4@gmail.com</p>
       <a href="https://www.google.com" target="_blank" rel="noreferrer">
