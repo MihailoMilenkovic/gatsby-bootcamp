9174d6e102bc8f50e509d70a0fb4899ac1ef53a4
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-08-26 17:50:46 +0200

    fix contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,9 +8,6 @@ const ContactPage = () => {
       <Head title="Contact" />
       <h1>Contact me</h1>
       <p>email: mihailo.milenkovic4@gmail.com</p>
-      <a href="https://www.google.com" target="_blank" rel="noreferrer">
-        idk
-      </a>
     </Layout>
   );
 };
